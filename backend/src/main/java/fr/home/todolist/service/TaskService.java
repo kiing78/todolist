9ce73111956010daf2c3235bc8978474339090b9
Kévin LORDINOT <kev97250@hotmail.fr>
2024-07-18 12:33:28 +0200

    add listTask functionnality

--- backend/src/main/java/fr/home/todolist/service/TaskService.java
+++ backend/src/main/java/fr/home/todolist/service/TaskService.java
@@ -2,6 +2,9 @@ package fr.home.todolist.service;
 
 import fr.home.todolist.entity.Task;
 
+import java.util.List;
+
 public interface TaskService<T> {
     public void addTask(T name);
+    public List<T> getTasks();
 }
