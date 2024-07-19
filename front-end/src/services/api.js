f30fd7c0ba8ba6d9a8deed920d44b60c211d1d49
Kévin LORDINOT <kev97250@hotmail.fr>
2024-07-19 16:20:27 +0200

    delete useless comment and add comment usefull

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api";
 
+//  POST request from API
 export const createItem=(item) =>{
     return axios.post("http://localhost:8080/api/new",item);
 };
 
+//  Get request from API
 export const getItem=()=>{
     return axios.get("http://localhost:8080/api/tasks");
 };
