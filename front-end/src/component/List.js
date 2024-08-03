039a371cc8b94f6e02b1c8de329bfef5927b8144
Kévin LORDINOT <kev97250@hotmail.fr>
2024-08-03 14:31:07 +0200

    add scrollbar, adaptive content list

--- front-end/src/component/List.js
+++ front-end/src/component/List.js
@@ -4,13 +4,13 @@ import trash from "../img/trash.svg";
 // Composant pour 
 const List = ({ taskList, onDelete }) => {
     return (
-        <div>
+        <div class="taskList">
             {taskList.length > 0 ? (
 
                 <ul>
                     {/* Afficher la liste des taches */}
                         {taskList.map((task) => (
-                    <div key={task.idTask} class="listTask">
+                    <div key={task.idTask} >
                         <li><p>{task.taskName}</p></li>
                         <button class="buttonDelete" onClick={() => onDelete(task.idTask)}><img src={trash} alt="Delete"></img></button>
                     </div>
